394599b28f29e702fdc8a90593772f00d817cf3a
TimurMakeev <timmak09@gmail.com>
2020-11-23 10:37:48 +0300

    Added brands logo's

--- src/Components/Brands/Brand.jsx
+++ src/Components/Brands/Brand.jsx
@@ -4,15 +4,15 @@ import style from "./Brands.module.css";
 import logo from '../../Assets/images/mercedes.png';
 
 const Brand = (props) => {
+  debugger
   return (
     <NavLink to="models">
       <div className={style.carItem}>
         <div className={style.logoWrapper}>
-          <div className={style.logo}><img src={logo}/></div>
+          <div className={style.logo}><img src={props.data.image}/></div>
         </div>
         <div className={style.title}>{props.data.title}</div>
       </div>
-      ;
     </NavLink>
   );
 };
