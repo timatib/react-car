3cf8cf28fa441b1ba5eaaaf5f169232b44140b37
timatib <tima.matsuka@gmail.com>
2020-12-01 16:01:11 +0300

    Refinemet of style pictures and work with searchNavbar

--- src/Components/common/NavBar/NavBar.jsx
+++ src/Components/common/NavBar/NavBar.jsx
@@ -1,19 +1,41 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import style from './NavBar.module.css'
-import logo from '../../../assets/images/logo.svg';
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import style from "./NavBar.module.css";
+import logo from "../../../assets/images/logo.svg";
 
-const NavBar = () => {
-return(
-    <div className={style.navBar}>
+const NavBar = (props) => {
+  let [inputValue, setInputValue] = useState(false);
+
+  let onSubmitFrom = (e) => {
+    if (e.key == "Enter") {
+      setInputValue(true);
+    }
+  };
+
+  let onСharacterЕracking = ({ target: { value } }) => {
+    if(value == ''){
+        setInputValue(false);
+    }
+  }
+
+  return (
+    <div className={style.navbarWrapper}>
+      <div className={style.navbar}>
         <div className={style.logoNavBar}>
-           <NavLink to='/'> <img src={logo} alt="img"/></NavLink>
+          <NavLink to="/">
+            {" "}
+            <img src={logo} alt="img" />
+          </NavLink>
         </div>
         <div className={style.searchNavBar}>
-            <input type="text" placeholder='Поиск' />
+          <input onKeyPress={onSubmitFrom} onChange={onСharacterЕracking} type="text" placeholder="Поиск" />
         </div>
+      </div>
+      <div className={!inputValue ? style.navSearch : `${style.navSearch} ${style.active}`}>
+            Рузультат поиска:
+      </div>
     </div>
-)
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
